92a2a3eb7aef1bef789884db33ffe712a9d57118
Jason Scharf <jasonlscharf@gmail.com>
2021-09-23 09:38:34 -0700

    Mv tick logic into BotRunner

--- src/roles/common-backend/bots/BotRunner.ts
+++ src/roles/common-backend/bots/BotRunner.ts
@@ -14,6 +14,7 @@ import { Money } from "../../common/numbers";
 import { Order, OrderState, OrderType } from "../../common/models/markets/Order";
 import { OrderEntity } from "../../common/entities/OrderEntity";
 import { PriceDataParameters } from "../../common/models/system/PriceDataParameters";
+import { PriceUpdateMessage } from "../messages/trading";
 import { RunState } from "../../common/models/system/RunState";
 import { TimeResolution } from "../../common/models/markets/TimeResolution";
 import { randomString } from "../../common/utils";
@@ -44,9 +45,98 @@ export interface IndicatorsAndSignals {
 }
 
 /**
- * Handles running of bots in backtest mode.
+ * Handles running of bots.
  */
 export class BotRunner {
+
+    /**
+     * Ticks a stateful trading bot.
+     * @param def 
+     * @param instanceRecord 
+     * @param price 
+     */
+    async tickBot(def: BotDefinition, instanceRecord: BotInstance, price: PriceUpdateMessage) {
+        const start = Date.now();
+        const run: BotRun = await strats.getLatestRunForInstance(instanceRecord.id);
+        const ctx = await buildBotContext(def, instanceRecord, run);
+
+
+        // TODO: Extract to BotRunner facility
+
+        const { genome } = ctx;
+        const symbolPair = instanceRecord.symbols;
+        const botType = genome.getGene<string>("META", "IMPL").value;
+        const res = genome.getGene<TimeResolution>("TIME", "RES").value;
+        const instance = botFactory.create(botType) as BotImplementation;
+
+        // Initialize new bots in a transaction to ensure we don't initialize it multiple times
+        if (instanceRecord.runState === RunState.INITIALIZING) {
+            let trx = await db.transaction();
+            try {
+                log.info(`Initializing ${botIdentifier(instanceRecord)}`);
+
+                const newState = await instance.initialize(ctx);
+
+                if (newState) {
+                    instanceRecord.stateJson = newState;
+                }
+
+                instanceRecord.runState = RunState.ACTIVE;
+                instanceRecord.prevTick = new Date();
+
+                await strats.updateBotInstance(instanceRecord, trx);
+                await trx.commit();
+            }
+            catch (err) {
+                log.error(`Error initializing ${botIdentifier(instanceRecord)}. Rolling back...`, err);
+
+                instanceRecord.runState = RunState.ERROR;
+                await trx.rollback();
+                await strats.updateBotInstance(instanceRecord);
+            }
+        }
+
+        if (instanceRecord.runState === RunState.ACTIVE) {
+            const maxHistoricals = genome.getGene<number>("TIME", "MI").value;
+            const now = Date.now();
+            const end = normalizePriceTime(res, new Date(now)).getTime();
+            const intervalMs = millisecondsPerResInterval(res);
+            const start = end - (intervalMs * maxHistoricals);
+
+            // Update price history. Note: This is *definitely* a case for optimization.
+            // Let's grab the previous N for now, until some sort of caching/progressive solution
+            // can be executed cross-node (b/c bots run on multiple machines)
+            const params: PriceDataParameters = {
+                exchange: env.PRIMO_DEFAULT_EXCHANGE,
+                res,
+                symbolPair,
+                fillMissing: true,
+                from: new Date(start),
+                to: new Date(end - 1),
+            };
+
+            // TODO: Consider price pull from API in the case of a gap?
+            //  Think about a deployment rollout
+            const prices = await sym.queryPricesForRange(params);
+            ctx.prices = prices;
+
+            const indicators = await instance.computeIndicatorsForTick(ctx, price);
+            const signal = await instance.computeSignal(ctx, price, indicators);
+            const tickState = await instance.tick(ctx, price, signal, indicators);
+
+            if (tickState !== null && instanceRecord.stateJson !== undefined) {
+                instanceRecord.stateJson = tickState;
+            }
+
+            instanceRecord.prevTick = new Date();
+            //log.debug(`Updating ${botIdentifier(instanceRecord)} in the DB...`);
+
+            await strats.updateBotInstance(instanceRecord);
+        }
+
+        const duration = Date.now() - start;
+        //log.debug(`Done ticking ${botIdentifier(instanceRecord)} in ${duration}ms`);
+    }
 
     /**
      * Computes signals and indicators for a backtest without executing any ordering logic.
@@ -65,7 +155,7 @@ export class BotRunner {
             const botType = genome.getGene<string>("META", "IMPL").value;
             const localInstance = botFactory.create(botType) as BotImplementation;
 
-            
+
             // Grab the prices + a run-in window of N prices before trading begins.
             // This is to support indicators with moving windows
             const intervalMs = millisecondsPerResInterval(args.res);
@@ -133,7 +223,7 @@ export class BotRunner {
                     if (foo && Array.isArray(foo)) {
                         cators.push(foo[foo.length - 1]);
                     }
-                    else{
+                    else {
                         cators.push(null);
                     }
                 }
