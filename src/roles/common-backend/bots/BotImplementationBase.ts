1dc6dae2cf3017b30df1e6b85477ae61ff5d8246
Jason Scharf <jasonlscharf@gmail.com>
2021-10-24 20:06:22 -0700

    Suppress state change logging during BT

--- src/roles/common-backend/bots/BotImplementationBase.ts
+++ src/roles/common-backend/bots/BotImplementationBase.ts
@@ -37,15 +37,14 @@ export class BotImplementationBase<TState = GeneticBotState> implements BotImple
         const { log } = ctx as any as BotContext<GeneticBotState>;
         const currState = genState.fsmState;
 
-        if (currState === GeneticBotFsmState.WAITING_FOR_SELL_OPP && newFsmState === GeneticBotFsmState.WAITING_FOR_BUY_ORDER_CONF) {
-            debugger;
-        }
-
         if (newFsmState !== currState) {
             genState.prevFsmState = currState;
             genState.prevFsmStateChangeTs = new Date();
             genState.fsmState = newFsmState;
-            log.info(`Bot '${botIdentifier(ctx.instance)}' changes state from '${genState.prevFsmState}' to '${genState.fsmState}'`);
+
+            if (ctx.instance.modeId !== "test-back") {
+                log.info(`Bot '${botIdentifier(ctx.instance)}' changes state from '${genState.prevFsmState}' to '${genState.fsmState}'`);
+            }
         }
 
         ctx.state = state;
